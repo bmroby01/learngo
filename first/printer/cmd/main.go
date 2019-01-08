e8c0c6923c11d6d371d399956d38b6a71870dd2e
bmroby01 <brianna@eltoro.com>
2019-01-08 13:50:25 -0500

    Added to library

---
+++ first/printer/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "learngo/first/printer"
+
+func main() {
+	printer.Hello()
+}
--- first/printer/printer.go
+++ first/printer/printer.go
@@ -1,3 +1,10 @@
 package printer
 
+import "fmt"
+
 //Creating a library and naming it
+
+//Hello is an exported function
+func Hello() {
+	fmt.Println("exported Hello")
+}
