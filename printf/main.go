2d960e3a49ff28d883828dedd3a9fedfb8875432
bmroby01 <brianna@eltoro.com>
2019-01-09 16:41:00 -0500

    Started printf exercises

---
+++ printf/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// EXERCISE: Print Your Age
+	//
+	//  Print your age using Prinft
+	//
+	// EXPECTED OUTPUT
+	//  I'm 30 years old.
+	//
+	// NOTE
+	//  You should change 30 to your age, of course.
+	// ---------------------------------------------------------
+
+	// ?
+
+	age := 33
+
+	fmt.Printf("I'm %d years old.\n", age)
+}
