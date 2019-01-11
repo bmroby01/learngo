d271610c65e31b2148cd1f805620af54e3dcac05
bmroby01 <brianna@eltoro.com>
2019-01-11 13:17:27 -0500

    Practicing with overflow

---
+++ Overflow/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		width  uint8 = 255
+		height       = 255
+	)
+
+	width++
+
+	if int(width) < height {
+		fmt.Println("height is greater than width")
+	}
+	fmt.Printf("width: %d height: %d\n", width, height)
+}
