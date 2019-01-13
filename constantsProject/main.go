d6c7975e3ad61aa9b1f4a56cc84a217b7f994c74
bmroby01 <brianna@eltoro.com>
2019-01-13 15:30:51 -0500

    Refactored project

--- constantsProject/main.go
+++ constantsProject/main.go
@@ -7,14 +7,16 @@ import (
 )
 
 func main() {
+
+	const (
+		feetInMeters float64 = 0.3048
+		feetInYards  float64 = feetInMeters / 0.9144
+	)
 	arg := os.Args[1]
 	feet, _ := strconv.ParseFloat(arg, 64)
 
-	const m = 0.3048
-	const f = 0.3333
-
-	meters := feet * m
-	yards := feet * f
+	meters := feet * feetInMeters
+	yards := feet * feetInYards
 
 	fmt.Printf("%g feet is %g meters.\n", feet, meters)
 	fmt.Printf("%g feet is %g yards.\n", feet, yards)
