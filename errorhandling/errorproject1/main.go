f467d81182bfb38755d300d6c56f1e86b24269b8
bmroby01 <brianna@eltoro.com>
2019-01-15 12:46:29 -0500

    First error handling project

---
+++ errorhandling/errorproject1/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const usage = `
+Feet to Meters
+--------------
+This program converts feet into meters.
+
+Usage:
+feet [feetsToConvert]`
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println(strings.TrimSpace(usage))
+		return
+	}
+
+	const (
+		feetInMeters = 0.3048
+		feetInYards  = feetInMeters / 0.9144
+	)
+
+	arg := os.Args[1]
+	feet, err := strconv.ParseFloat(arg, 64)
+
+	meters := feet * feetInMeters
+	yards := math.Round(feet * feetInYards)
+
+	if err != nil {
+		fmt.Printf("%q is not a number.\n", arg)
+		return
+	}
+
+	fmt.Printf("%g feet is %g meters.\n", feet, meters)
+	fmt.Printf("%g feet is %g yards.\n", feet, yards)
+}
